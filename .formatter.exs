6bb9043823d28bffe0c16df44c1f09ec7d806b43
Rafael Willians <me@rwillians.com>
2026-01-21 12:47:20 -0300

    chore: removed unused macro

--- .formatter.exs
+++ .formatter.exs
@@ -2,12 +2,10 @@
   inputs: ["{lib,test}/**/*.{ex,exs}"],
   line_length: 180,
   locals_without_parens: [
-    defmodifiers: 1,
     deftype: 1
   ],
   export: [
     locals_without_parens: [
-      defmodifiers: 1,
       deftype: 1
     ]
   ]
